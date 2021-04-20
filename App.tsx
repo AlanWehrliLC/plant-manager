f7bff41d89cf8c933b8a87098d21e36630c7190c
Alan Wehrli <64156830+AlanWehrliLC@users.noreply.github.com>
2021-04-20 01:50:08 -0300

    Project started, and welcome page prepared

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import { Welcome } from './src/pages/Welcome'
+
+export default function App() {
+  return (
+    <>
+      <Welcome/>
+    </>
+  )
+}
+
+const style = StyleSheet.create({
+  conteiner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Text, TouchableOpacity, StyleSheet, TouchableOpacityProps } from 'react-native'
+import colors from '../styles/colors'
+
+interface ButtonProps extends TouchableOpacityProps{
+    title: string,
+
+}
+
+export function Button({ title, ...rest }: ButtonProps) {
+    return (
+        <TouchableOpacity style={style.button} {...rest}>
+            <Text style={style.buttonText}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const style = StyleSheet.create({
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 20,
+        height: 56,
+        width: 56,
+    },
+    buttonText: {
+        color: colors.white,
+        fontSize: 24
+    }
+})
